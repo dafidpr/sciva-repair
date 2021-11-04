455cb44170486cd135aa87a94a70df10cf2eed41
daqoda <daqoda09@gmail.com>
2021-11-04 19:03:07 +0700

    make migration

--- database/migrations/2021_11_03_002320_create_products_table.php
+++ database/migrations/2021_11_03_002320_create_products_table.php
@@ -15,6 +15,13 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('barcode', 255);
+            $table->string('name', 255);
+            $table->decimal('selling_price');
+            $table->decimal('purchase_price');
+            $table->decimal('member_price');
+            $table->integer('stock');
+            $table->integer('limit');
             $table->timestamps();
         });
     }
