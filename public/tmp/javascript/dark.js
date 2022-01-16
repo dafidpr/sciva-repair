bfc53966128a48a409f7b58e0c0c98f0b19c84b6
daqoda <daqoda09@gmail.com>
2022-01-16 21:56:54 +0700

    revisi 4

--- public/tmp/javascript/dark.js
+++ public/tmp/javascript/dark.js
@@ -4,17 +4,16 @@ if(localStorage.getItem('theme') == 'dark'){
 
 function dark_mode(isDark){
     if(isDark){
-        $('#bootstrap-style').attr("href","http://localhost:8000/tmp/assets/css/bootstrap-dark.min.css")
-        $('#app-style').attr("href","http://localhost:8000/tmp/assets/css/app-dark.min.css")
+        $('#bootstrap-style').attr("href","/tmp/assets/css/bootstrap-dark.min.css")
+        $('#app-style').attr("href","/tmp/assets/css/app-dark.min.css")
 
 
         document.getElementById('btn-dark-mode').innerHTML = `<button class="btn btn-sm-success" onclick="dark_mode(false)"><i class="fas fa-sun"></i></button>`
 
         localStorage.setItem('theme', 'dark')
     }else{
-        document.getElementById('dark').innerHTML = `<link href="http://localhost:8000/tmp/assets/css/bootstrap.min.css" id="bootstrap-style" rel="stylesheet" type="text/css">
-        <!-- App Css-->
-        <link href="http://localhost:8000/tmp/assets/css/app.min.css" id="app-style" rel="stylesheet" type="text/css">`
+        $('#bootstrap-style').attr("href","/tmp/assets/css/bootstrap.min.css")
+        $('#app-style').attr("href","/tmp/assets/css/app.min.css")
 
         document.getElementById('btn-dark-mode').innerHTML = `<button class="btn btn-sm-success" onclick="dark_mode(true)"><i class="fas fa-moon"></i></button>`
         localStorage.removeItem('theme')
